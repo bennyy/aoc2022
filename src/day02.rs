45c6f3d45ff49faa70269d55ab9a89f0390332a2
Benjamin Andersson <4161184+bennyy@users.noreply.github.com>
2022-12-02 18:03:45 +0100

    Day 2 Completed

--- src/day02.rs
+++ src/day02.rs
@@ -1,14 +1,121 @@
-use crate::{aoc::AdventOfCode, not_implemented};
+use crate::aoc::AdventOfCode;
 
-pub struct Day2 {}
+pub struct Day2 {
+    puzzle1_result: i32,
+    puzzle2_result: i32,
+}
 
-not_implemented!(Day2, "day02");
+impl Day2 {
+    pub fn new() -> Day2 {
+        Day2 {
+            puzzle1_result: 0,
+            puzzle2_result: 0,
+        }
+    }
+
+    pub fn translate_hand(&self, action: char) -> char {
+        match action {
+            'X' => 'A',
+            'Y' => 'B',
+            'Z' => 'C',
+            _ => panic!("Bad input"),
+        }
+    }
+
+    pub fn winning_action(&self, action: char) -> char {
+        match action {
+            'A' => 'B',
+            'B' => 'C',
+            'C' => 'A',
+            _ => panic!("Bad input"),
+        }
+    }
+
+    pub fn lose_action(&self, action: char) -> char {
+        match action {
+            'A' => 'C',
+            'B' => 'A',
+            'C' => 'B',
+            _ => panic!("Bad input"),
+        }
+    }
+
+    pub fn translate_hand_v2(&self, elf_hand: char, my_action: char) -> char {
+        match my_action {
+            'X' => self.lose_action(elf_hand),
+            'Y' => elf_hand,
+            'Z' => self.winning_action(elf_hand),
+            _ => panic!("Bad input"),
+        }
+    }
+
+    pub fn get_point(&self, hand: char) -> i32 {
+        match hand {
+            'A' => 1, // Stone
+            'B' => 2, // Paper
+            'C' => 3, // Scissors
+            _ => 0,
+        }
+    }
+
+    pub fn get_score(&self, elf_hand: char, my_hand: char) -> i32 {
+        if elf_hand == my_hand {
+            self.get_point(elf_hand) + 3
+        } else if self.winning_action(elf_hand) == my_hand {
+            self.get_point(my_hand) + 6
+        } else {
+            self.get_point(my_hand)
+        }
+    }
+}
+
+impl AdventOfCode for Day2 {
+    fn day_str(&self) -> String {
+        "day02".to_owned()
+    }
+
+    fn run_puzzle1(&mut self, input_str: String) {
+        let rps_rounds: Vec<&str> = input_str.split('\n').collect();
+
+        let mut score: i32 = 0;
+        for rps in rps_rounds {
+            let mut game = rps.chars().filter(|c| !c.is_whitespace());
+            let elf_hand = game.next().unwrap();
+            let my_hand = self.translate_hand(game.next().unwrap());
+
+            score += self.get_score(elf_hand, my_hand);
+        }
+        self.puzzle1_result = score;
+    }
+
+    fn run_puzzle2(&mut self, input_str: String) {
+        let rps_rounds: Vec<&str> = input_str.split('\n').collect();
+
+        let mut score: i32 = 0;
+        for rps in rps_rounds {
+            let mut game = rps.chars().filter(|c| !c.is_whitespace());
+            let elf_hand = game.next().unwrap();
+            let my_hand = self.translate_hand_v2(elf_hand, game.next().unwrap());
+
+            score += self.get_score(elf_hand, my_hand);
+        }
+        self.puzzle2_result = score;
+    }
+
+    fn get_puzzle1_result(&self) -> i32 {
+        self.puzzle1_result
+    }
+
+    fn get_puzzle2_result(&self) -> i32 {
+        self.puzzle2_result
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{puzzle1_test, puzzle2_test};
 
-    puzzle1_test!(Day2, 0, 0);
-    puzzle2_test!(Day2, 0, 0);
+    puzzle1_test!(Day2, 15, 11475);
+    puzzle2_test!(Day2, 12, 16862);
 }
