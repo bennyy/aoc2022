95ec2feb7095e19a22cfd0a2af2a5493d9b4cebf
Benjamin Andersson <4161184+bennyy@users.noreply.github.com>
2022-12-10 09:42:47 +0100

    Add Day8,9,10 template

--- src/day08.rs
+++ src/day08.rs
@@ -1,17 +1,36 @@
-use crate::{aoc::AdventOfCode, not_implemented};
+use crate::{aoc::AdventOfCode, default_aoc_struct, default_new_ctor};
 use std::any::Any;
-
-pub struct Day8 {}
+default_aoc_struct!(Day8, i32);
+default_new_ctor!(Day8);
 
 impl Day8 {}
 
-not_implemented!(Day8, "day08");
+impl AdventOfCode for Day8 {
+    fn day_str(&self) -> String {
+        "day08".to_owned()
+    }
+
+    fn run_puzzle1(&mut self, input_str: String) {
+    }
+
+    fn run_puzzle2(&mut self, input_str: String) {
+    }
+
+    fn get_puzzle1_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle1_result))
+    }
+
+    fn get_puzzle2_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle2_result))
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{puzzle1_test, puzzle2_test};
 
-    puzzle1_test!(Day8, 0, 0);
+    puzzle1_test!(Day8, 0 /* 21 */, 0);
     puzzle2_test!(Day8, 0, 0);
 }
