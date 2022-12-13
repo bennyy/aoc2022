9fa65a23453a767170a53f845610b70ede375adf
Benjamin Andersson <4161184+bennyy@users.noreply.github.com>
2022-12-13 16:28:56 +0100

    Add Day 12 and 13

--- src/day12.rs
+++ src/day12.rs
@@ -1,17 +1,37 @@
-use crate::{aoc::AdventOfCode, not_implemented};
+use crate::{aoc::AdventOfCode, default_aoc_struct, default_new_ctor};
 use std::any::Any;
+default_aoc_struct!(Day12, i32);
+default_new_ctor!(Day12);
 
-pub struct Day12 {}
 
-impl Day12 {}
+impl Day12 {
+}
 
-not_implemented!(Day12, "day12");
+impl AdventOfCode for Day12 {
+    fn day_str(&self) -> String {
+        "day12".to_owned()
+    }
+
+    fn run_puzzle1(&mut self, input_str: String) {
+    }
+
+    fn run_puzzle2(&mut self, input_str: String) {
+    }
+
+    fn get_puzzle1_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle1_result))
+    }
+
+    fn get_puzzle2_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle2_result))
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{puzzle1_test, puzzle2_test};
 
-    puzzle1_test!(Day12, 0, 0);
+    puzzle1_test!(Day12, 31, 0);
     puzzle2_test!(Day12, 0, 0);
 }
--- src/day13.rs
+++ src/day13.rs
@@ -1,17 +1,37 @@
-use crate::{aoc::AdventOfCode, not_implemented};
+use crate::{aoc::AdventOfCode, default_aoc_struct, default_new_ctor};
 use std::any::Any;
+default_aoc_struct!(Day13, i32);
+default_new_ctor!(Day13);
 
-pub struct Day13 {}
 
-impl Day13 {}
+impl Day13 {
+}
 
-not_implemented!(Day13, "day13");
+impl AdventOfCode for Day13 {
+    fn day_str(&self) -> String {
+        "day13".to_owned()
+    }
+
+    fn run_puzzle1(&mut self, input_str: String) {
+    }
+
+    fn run_puzzle2(&mut self, input_str: String) {
+    }
+
+    fn get_puzzle1_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle1_result))
+    }
+
+    fn get_puzzle2_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle2_result))
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{puzzle1_test, puzzle2_test};
 
-    puzzle1_test!(Day13, 0, 0);
+    puzzle1_test!(Day13, 0 /* 13 */, 0);
     puzzle2_test!(Day13, 0, 0);
 }
