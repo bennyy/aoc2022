9a8df64afba38c26c687e85d5664ac40a157311a
Benjamin Andersson <4161184+bennyy@users.noreply.github.com>
2022-12-16 21:33:49 +0100

    Day 14 part 2 completed.. slow

--- src/day14.rs
+++ src/day14.rs
@@ -29,6 +29,22 @@ impl Day14 {
         }
         neighbors
     }
+    fn get_roof(current_index: usize, width: usize, cave_length: usize) -> Vec<usize> {
+        let directions: Vec<(i32, i32)> = vec![(1, 0) /*down */];
+        let mut neighbors: Vec<usize> = Vec::new();
+        for v in directions {
+            let x = (current_index % width) as i32 + v.0;
+            let y = (current_index / width) as i32 + v.1;
+
+            if x >= width as i32 || x < 0 || y < 0 || y >= cave_length as i32 / width as i32 {
+                // Outside the limit..
+                continue;
+            }
+            let i = (x as u32) + (width as u32) * (y as u32);
+            neighbors.push(i as usize);
+        }
+        neighbors
+    }
 
     fn index_to_coord(index: usize, width: usize) -> (usize, usize) {
         let x = index % width;
@@ -202,7 +218,153 @@ impl AdventOfCode for Day14 {
         self.puzzle1_result = cave.iter().filter(|&n| *n == SAND).count() as i32;
     }
 
-    fn run_puzzle2(&mut self, _input_str: String) {}
+    fn run_puzzle2(&mut self, input_str: String) {
+        let mut rock_lines: Vec<Vec<(u32, u32)>> = input_str
+            .lines()
+            .map(|line| {
+                let coords = line
+                    .split(" -> ")
+                    .map(|co| {
+                        let mut xy = co.split(',');
+                        let x = xy.next().unwrap().parse::<u32>().unwrap();
+                        let y = xy.next().unwrap().parse::<u32>().unwrap();
+                        (x, y)
+                    })
+                    .collect();
+                coords
+            })
+            .collect();
+
+        let mut max_x = 0;
+        let mut max_y = 0;
+
+        for rock_line in rock_lines.iter() {
+            for rock_coord in rock_line {
+                if rock_coord.0 > max_x {
+                    max_x = rock_coord.0;
+                }
+                if rock_coord.1 > max_y {
+                    max_y = rock_coord.1;
+                }
+            }
+        }
+
+        max_x = 1000;
+        max_y += 2;
+        let mut bottom_line: Vec<(u32, u32)> = Vec::new();
+        bottom_line.push((0, max_y));
+        bottom_line.push((800, max_y));
+
+        rock_lines.push(bottom_line);
+
+        let height = max_y as usize + 1;
+        let width = max_x as usize + 1;
+
+        let total_size = height * width;
+        let mut cave = vec![AIR; total_size as usize];
+
+        for rock_lines_ in rock_lines.iter() {
+            for rock_line in rock_lines_.windows(2) {
+                let start_x = (rock_line[0].0) as usize;
+                let start_y = (rock_line[0].1) as usize;
+                let end_x = (rock_line[1].0) as usize;
+                let end_y = (rock_line[1].1) as usize;
+
+                let start_i = (start_x + width * (start_y)) as usize;
+                let end_i = (end_x + width * end_y) as usize;
+
+                if start_x == end_x {
+                    let steps = usize::abs_diff(start_y, end_y) as usize + 1;
+                    let min = cmp::min(start_i, end_i) as usize;
+                    cave[min..]
+                        .iter_mut()
+                        .step_by(width as usize)
+                        .take(steps)
+                        .for_each(|x| *x = 1);
+                } else if start_y == end_y {
+                    let diff = usize::abs_diff(end_i, start_i) + 1;
+                    let min = cmp::min(start_i, end_i) as usize;
+                    cave[min..min + diff].fill(1);
+                } else {
+                    panic!("Wrong coordinate...");
+                }
+            }
+        }
+
+        let rock_start_index = 500;
+        *cave.get_mut(rock_start_index).unwrap() = 0;
+
+        let cave_length = cave.len();
+        let mut flood_done = false;
+        while !flood_done {
+            //for _ in 0..94 {
+            let mut current_sand = rock_start_index;
+
+            // Clear path from the map, useful for debugging.
+            cave.iter_mut()
+                .filter(|n| **n == PATH)
+                .for_each(|x| *x = AIR);
+
+            loop {
+                let neighbors = Day14::get_neighbours(current_sand, width, cave_length);
+
+                *cave.get_mut(current_sand).unwrap() = 99;
+
+                let mut iter = neighbors.iter();
+
+                let down = *iter.next().unwrap();
+                let down_left = *iter.next().unwrap();
+                let down_right = *iter.next().unwrap();
+
+                let cave_down = *cave.get(down).unwrap();
+                let cave_down_left = *cave.get(down_left).unwrap();
+                let cave_down_right = *cave.get(down_right).unwrap();
+
+                if current_sand == rock_start_index
+                    && cave_down == SAND
+                    && cave_down_left == SAND
+                    && cave_down_right == SAND
+                {
+                    flood_done = true;
+                }
+
+                if cave_down == AIR {
+                    // If we can go down, go down!!
+                    current_sand = down;
+                    continue;
+                }
+
+                // Next step is a wall or sand.
+                if cave_down == WALL || cave_down == SAND {
+                    // The unit of sand attempts to instead move diagonally
+                    // one step down and to the left.
+                    if cave_down_left == AIR {
+                        current_sand = down_left;
+                        continue;
+                    }
+
+                    //If that tile is blocked, the unit of sand attempts to
+                    // instead move diagonally one step down and to the right
+                    if cave_down_right == AIR {
+                        current_sand = down_right;
+                        continue;
+                    }
+
+                    // If all three possible destinations are blocked,
+                    //the unit of sand comes to rest and no longer moves
+                    if cave_down > AIR && cave_down_left > AIR && cave_down_right > AIR {
+                        *cave.get_mut(current_sand).unwrap() = 2;
+                        break;
+                    }
+                }
+
+                panic!("Not implemented");
+            }
+            //Day14::print_cave(&cave, width);
+        }
+
+        self.puzzle2_result = cave.iter().filter(|&n| *n == SAND).count() as i32;
+    }
 
     fn get_puzzle1_result(&self) -> Option<Box<dyn Any>> {
         Some(Box::new(self.puzzle1_result))
@@ -219,5 +381,5 @@ mod tests {
     use crate::{puzzle1_test, puzzle2_test};
 
     puzzle1_test!(Day14, 24, 719);
-    puzzle2_test!(Day14, 0, 0);
+    puzzle2_test!(Day14, 93, 23390);
 }
