7c87dc30fa8ffa05094549051614ab353de24670
Benjamin Andersson <4161184+bennyy@users.noreply.github.com>
2022-12-21 17:16:35 +0100

    Add day 20 and 21 input

--- src/day21.rs
+++ src/day21.rs
@@ -1,17 +1,33 @@
-use crate::{aoc::AdventOfCode, not_implemented};
+use crate::{aoc::AdventOfCode, default_aoc_struct, default_new_ctor};
 use std::any::Any;
-
-pub struct Day21 {}
+default_aoc_struct!(Day21, i32);
+default_new_ctor!(Day21);
 
 impl Day21 {}
 
-not_implemented!(Day21, "day21");
+impl AdventOfCode for Day21 {
+    fn day_str(&self) -> String {
+        "day21".to_owned()
+    }
+
+    fn run_puzzle1(&mut self, _input_str: String) {}
+
+    fn run_puzzle2(&mut self, _input_str: String) {}
+
+    fn get_puzzle1_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle1_result))
+    }
+
+    fn get_puzzle2_result(&self) -> Option<Box<dyn Any>> {
+        Some(Box::new(self.puzzle2_result))
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{puzzle1_test, puzzle2_test};
 
-    puzzle1_test!(Day21, 0, 0);
+    puzzle1_test!(Day21, 0 /* 152 */, 0);
     puzzle2_test!(Day21, 0, 0);
 }
